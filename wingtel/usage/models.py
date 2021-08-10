2f8f1c9fde3db3079cd951c8d2dade92b9a761fe
t1m4 <ruslan.hamidullin@starnavi.io>
2021-08-10 11:46:22 +0300

    Add type_of_subscription in BothUsageRecord model

--- wingtel/usage/models.py
+++ wingtel/usage/models.py
@@ -28,8 +28,13 @@ class BothUsageRecord(models.Model):
         ('data', 'DataUsage'),
         ('voice', 'VoiceUsage'),
     )
+    SUBSCRIPTION_TYPE = Choices(
+        ('att', 'Att'),
+        ('sprint', 'Sprint'),
 
+    )
     subscription_id = models.IntegerField()
+    type_of_subscription = models.CharField(max_length=100, choices=SUBSCRIPTION_TYPE)
     type_of_usage = models.CharField(max_length=100, choices=USAGE_TYPES)
     price = models.DecimalField(decimal_places=2, max_digits=7, default=0)
     usage_date = models.DateField(null=False)
