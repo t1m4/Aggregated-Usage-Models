99e5bd75650d0478c4c2ec0d0f10cf1f0f2d49a5
Ruslan Hamidullin <ruslan.hamidullin@starnavi.io>
2021-08-11 17:55:08 +0300

    Add tests for TestUsageMetrics view

--- wingtel/usage/tests/test_views.py
+++ wingtel/usage/tests/test_views.py
@@ -19,7 +19,6 @@ class TestSubscriptionExceededPrice(TestCase):
         create_subscriptions(user, count=4)
         fill_models(count=2)
 
-
     def test_cannot_get_without_params(self):
         response = self.client.get(self.price_limit_url)
         assert response.status_code == 400
@@ -68,87 +67,55 @@ class TestSubscriptionExceededPrice(TestCase):
         params['type_of_subscription'] = 'sprint'
         response = self.client.get(self.price_limit_url, self.params)
         assert response.status_code == 200
-        print(BothUsageRecord.objects.all().count())
         assert len(response.json()) == 4
 
 
-# class TestUsageMetrics(TestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         """
-#         setup data for whole class
-#         """
-#         cls.usage_metrics_url = reverse('usage-metrics', args=[10])
-#         cls.aggregate_url = reverse('usage-aggregate')
-#         cls.params = {'price_limit': 100, 'sub_type': 'att'}
-#         user = User.objects.create(username='test', password='test')
-#         create_subscriptions(user)
-#         fill_models(count=2)
-#
-#     def setUp(self):
-#         self.client.get(self.aggregate_url, {'type': 'data'})
-#         self.client.get(self.aggregate_url, {'type': 'voice'})
-#         self.params = {'from': '2019-1-1', 'to': '2019-1-2', 'usage_type': 'data', 'sub_type': 'att'}
-#
-#     def test_cannot_get_without_params(self):
-#         response = self.client.get(self.usage_metrics_url)
-#         assert response.status_code == 404
-#
-#     def test_cannot_get_without_from(self):
-#         params = self.params.copy()
-#         del params['from']
-#         response = self.client.get(self.usage_metrics_url, params)
-#         assert response.status_code == 404
-#
-#     def test_cannot_get_without_to(self):
-#         params = self.params.copy()
-#         del params['to']
-#         response = self.client.get(self.usage_metrics_url, params)
-#         assert response.status_code == 404
-#
-#     def test_cannot_get_without_usage_type(self):
-#         params = self.params.copy()
-#         del params['usage_type']
-#         response = self.client.get(self.usage_metrics_url, params)
-#         assert response.status_code == 404
-#
-#     def test_cannot_get_without_sub_type(self):
-#         params = self.params.copy()
-#         del params['sub_type']
-#         response = self.client.get(self.usage_metrics_url, params)
-#         assert response.status_code == 404
-#
-#     def test_cannot_get_with_invalid_from(self):
-#         params = self.params.copy()
-#         params['from'] = "invalid"
-#         response = self.client.get(self.usage_metrics_url, params)
-#         assert response.status_code == 404
-#
-#     def test_cannot_get_with_invalid_to(self):
-#         params = self.params.copy()
-#         params['to'] = "invalid"
-#         response = self.client.get(self.usage_metrics_url, params)
-#         assert response.status_code == 404
-#
-#     def test_cannot_get_with_invalid_usage_type(self):
-#         params = self.params.copy()
-#         params['usage_type'] = "invalid"
-#         response = self.client.get(self.usage_metrics_url, params)
-#         assert response.status_code == 404
-#
-#     def test_cannot_get_with_invalid_sub_type(self):
-#         params = self.params.copy()
-#         params['sub_type'] = "invalid"
-#         response = self.client.get(self.usage_metrics_url, params)
-#         assert response.status_code == 404
-#
-#     def test_can_get_with_params(self):
-#         response = self.client.get(self.usage_metrics_url, self.params)
-#         assert len(response.json()) == 3
-#         assert response.status_code == 200
-#
-#     def test_get_empty_response_with_not_exits_subscription(self):
-#         usage_metrics_url = reverse('usage-metrics', args=[100])
-#         response = self.client.get(usage_metrics_url, self.params)
-#         assert response.json() == []
-#         assert response.status_code == 200
+class TestUsageMetrics(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        """
+        setup data for whole class
+        """
+        cls.usage_metrics_url = reverse('usage-metrics', args=[15])
+        user = User.objects.create(username='test', password='test')
+        create_subscriptions(user)
+        fill_models(count=3)
+        # fill_models()
+
+    def setUp(self):
+        self.params = {'usage_date__gte': '2019-1-1', 'usage_date__lte': '2019-1-2', 'type_of_usage': 'data',
+                       'type_of_subscription': 'att'}
+
+    def test_can_get_without_params(self):
+        response = self.client.get(self.usage_metrics_url)
+        assert response.status_code == 200
+
+    def test_cannot_get_without_invalid_usage_type(self):
+        params = self.params.copy()
+        params['type_of_usage'] = 'invalid'
+        response = self.client.get(self.usage_metrics_url, params)
+        assert response.status_code == 400
+
+    def test_cannot_get_without_invalid_type_of_subscription(self):
+        params = self.params.copy()
+        params['type_of_subscription'] = 'invalid'
+        response = self.client.get(self.usage_metrics_url, params)
+        assert response.status_code == 400
+
+    def test_cannot_get_without_invalid_usage_date__gte(self):
+        params = self.params.copy()
+        params['usage_date__gte'] = 'invalid'
+        response = self.client.get(self.usage_metrics_url, params)
+        assert response.status_code == 400
+
+    def test_cannot_get_without_invalid_usage_date__lte(self):
+        params = self.params.copy()
+        params['usage_date__lte'] = 'invalid'
+        response = self.client.get(self.usage_metrics_url, params)
+        assert response.status_code == 400
+
+    def test_get_empty_response_with_not_exits_subscription(self):
+        usage_metrics_url = reverse('usage-metrics', args=[100])
+        response = self.client.get(usage_metrics_url, self.params)
+        assert response.json() == []
+        assert response.status_code == 200
